68cf86a12373087cdc5696291b1ac0a55caf0839
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-23 15:40:05 +0200

    delete quote component --> useless

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Quote from "../quote/Quote";
 import Quotes from "../quotes/Quotes";
 import Header from "../header/Header";
 
@@ -10,16 +9,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Quote
-          quote="I believe the children are the future... Unless we stop them now!"
-          character="Homer Simpson"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
-        <Quote
-          quote="Me fail English? That's unpossible"
-          character="Ralph Wiggum"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
         <Quotes />
       </div>
     );
